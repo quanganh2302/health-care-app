1bd96ff7a0eac579cc4c7663b3d99a2d88519553
Quang Anh <anh.doanquang98@gmail.com>
2023-06-07 14:49:33 +0700

    Little change

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -4,6 +4,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("testdata", "root", null, {
   host: "localhost",
   dialect: "mysql",
+  logging: false
 });
 
 let connectDB = async () => {
