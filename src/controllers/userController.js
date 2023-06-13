703678b5fc970455db09e76fd8db3b6a9ef07196
Quang Anh <anh.doanquang98@gmail.com>
2023-06-14 01:52:19 +0700

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,7 +16,17 @@ let handleLogin = async (req, res) => {
     user: userData.user ? userData.user : {},
   });
 };
-
+let handleGetAllUsers = async (req, res) => {
+  let id = req.body.id; //ALL, ID
+  let users = await userServices.getAllUser(id);
+  console.log(users)
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "ok",
+    users
+  });
+};
 module.exports = {
   handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
 };
